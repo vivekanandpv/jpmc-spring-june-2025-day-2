c09e4f101504b8ed2d1f3484cda925dd5c3fe40c
Vivekanand Pujar <vivekanandpv@gmail.com>
2025-06-17 14:54:33 +0530

    done

---
+++ src/main/java/in/athenaeum/jpmcspringjune2025day2/dataaccess/CustomerRowMapper.java
@@ -0,0 +1,22 @@
+package in.athenaeum.jpmcspringjune2025day2.dataaccess;
+
+import in.athenaeum.jpmcspringjune2025day2.models.Customer;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CustomerRowMapper implements RowMapper<Customer> {
+    @Override
+    public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Customer customer = new Customer();
+        
+        customer.setCustomerId(rs.getInt("customer_id"));
+        customer.setFirstName(rs.getString("first_name"));
+        customer.setLastName(rs.getString("last_name"));
+        customer.setEmail(rs.getString("email"));
+        customer.setCity(rs.getString("city"));
+        
+        return customer;
+    }
+}
