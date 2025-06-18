e751080a43a9e4a99b59f0f24a67a0b39a5aa2bb
Vivekanand Pujar <vivekanandpv@gmail.com>
2025-06-18 12:34:32 +0530

    done

--- src/main/java/in/athenaeum/jpmcspringjune2025day2/services/CustomerServiceJpaImplementation.java
+++ src/main/java/in/athenaeum/jpmcspringjune2025day2/services/CustomerServiceJpaImplementation.java
@@ -1,9 +1,12 @@
 package in.athenaeum.jpmcspringjune2025day2.services;
 
+import in.athenaeum.jpmcspringjune2025day2.exceptions.RecordNotFoundException;
+import in.athenaeum.jpmcspringjune2025day2.models.Customer;
 import in.athenaeum.jpmcspringjune2025day2.repositories.CustomerJpaRepository;
 import in.athenaeum.jpmcspringjune2025day2.viewmodels.CustomerCreateViewModel;
 import in.athenaeum.jpmcspringjune2025day2.viewmodels.CustomerUpdateViewModel;
 import in.athenaeum.jpmcspringjune2025day2.viewmodels.CustomerViewModel;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,26 +21,54 @@ public class CustomerServiceJpaImplementation implements CustomerService {
 
     @Override
     public List<CustomerViewModel> getAll() {
-        return List.of();
+        return customerJpaRepository
+                .findAll()
+                .stream()
+                .map(this::toViewModel)
+                .toList();
     }
 
     @Override
     public CustomerViewModel getById(int customerId) {
-        return null;
+        return toViewModel(fromId(customerId));
     }
 
     @Override
     public CustomerViewModel create(CustomerCreateViewModel viewModel) {
-        return null;
+        return toViewModel(customerJpaRepository.saveAndFlush(toDomain(viewModel)));
     }
 
     @Override
     public CustomerViewModel update(int customerId, CustomerUpdateViewModel viewModel) {
-        return null;
+        Customer customerDb = fromId(customerId);
+                
+        customerDb.setFirstName(viewModel.getFirstName());
+        customerDb.setLastName(viewModel.getLastName());
+        customerDb.setCity(viewModel.getCity());
+
+        return toViewModel(customerJpaRepository.saveAndFlush(customerDb));
     }
 
     @Override
     public void deleteById(int customerId) {
+        customerJpaRepository.delete(fromId(customerId));
+    }
+    
+    private Customer fromId(int customerId) {
+        return customerJpaRepository
+                .findById(customerId)
+                .orElseThrow(() -> new RecordNotFoundException("Could not find the customer: " + customerId));
+    }
 
+    private CustomerViewModel toViewModel(Customer customer) {
+        CustomerViewModel viewModel = new CustomerViewModel();
+        BeanUtils.copyProperties(customer, viewModel);
+        return viewModel;
+    }
+
+    private Customer toDomain(CustomerCreateViewModel viewModel) {
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(viewModel, customer);
+        return customer;
     }
 }
